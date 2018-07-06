f7231b6b553981b1c08baea109fb12598f547f2b
damianlzy <damian@etechnologycentre.com>
2018-07-06 16:20:42 +0800

    permissions

--- app/src/main/java/com/example/trueno/redproject/LandingPage.java
+++ app/src/main/java/com/example/trueno/redproject/LandingPage.java
@@ -1,8 +1,15 @@
 package com.example.trueno.redproject;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Html;
@@ -36,6 +43,9 @@ import com.google.firebase.auth.GoogleAuthProvider;
 import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LandingPage extends AppCompatActivity {
     public static final String TAG = "LandingPage.java";
 
@@ -47,6 +57,9 @@ public class LandingPage extends AppCompatActivity {
 
     private static final String EMAIL = "email";
     private FirebaseAuth mAuth;
+
+    private String[] permissions = {Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission
+            .ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION,Manifest.permission.CALL_PHONE};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,6 +79,15 @@ public class LandingPage extends AppCompatActivity {
         mCallbackManager = CallbackManager.Factory.create();
         final LoginButton loginButton = findViewById(R.id.btnFacebook);
         loginButton.setReadPermissions("email", "public_profile");
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if(arePermissionsEnabled()){
+//                    permissions granted, continue flow normally
+            }else{
+                requestMultiplePermissions();
+            }
+        }
+
 
         loginButton.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
             @Override
@@ -216,5 +238,78 @@ public class LandingPage extends AppCompatActivity {
                     }
                 });
     }
+
+
+    public boolean arePermissionsEnabled() {
+
+        if (Build.VERSION.SDK_INT >= 23) {
+//            if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+//                    == PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+//                    == PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.CAMERA)
+//                    == PackageManager.PERMISSION_GRANTED) {
+//                Log.v(TAG, "Permission is granted");
+//                return true;
+//            } else {
+//                ActivityCompat.requestPermissions(this, new String[]{
+//                        Manifest.permission.CAMERA
+//                }, 1);
+//                return false;
+//            }
+            for(String permission : permissions){
+                if(checkSelfPermission(permission) != PackageManager.PERMISSION_GRANTED)
+                    return false;
+            }
+            return true;
+
+
+        } else { //permission is automatically granted on sdk<23 upon installation
+            Log.v(TAG, "Permission is granted");
+            return true;
+        }
+
+    }
+
+    private void requestMultiplePermissions(){
+        List<String> remainingPermissions = new ArrayList<>();
+        for (String permission : permissions) {
+            if (ContextCompat.checkSelfPermission(this.getApplicationContext(),permission) != PackageManager.PERMISSION_GRANTED) {
+                remainingPermissions.add(permission);
+            }
+        }
+        ActivityCompat.requestPermissions(this,remainingPermissions.toArray(new String[remainingPermissions.size()]), 101);
+
+
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,@NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == 101){
+            for(int i=0;i<grantResults.length;i++){
+                if(ActivityCompat.shouldShowRequestPermissionRationale(LandingPage.this,permissions[i])){
+                    new AlertDialog.Builder(this)
+                            .setMessage("Permissions are required to use the application")
+                            .setPositiveButton("Allow", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    requestMultiplePermissions();
+                                }
+                            })
+                            .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    requestMultiplePermissions();
+                                }
+                            })
+                            .create()
+                            .show();
+                }
+                return;
+            }
+            //all is good, continue flow
+        }
+    }
+
 
 }
